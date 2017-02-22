149eed9642d0a7ed94540fe61084af98b2c528ab
Jakub Roman <jakub@jakubroman.sk>
2017-02-23 00:08:28 +0100

    nuget

--- webhoseio.Tests/Properties/AssemblyInfo.cs
+++ webhoseio.Tests/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("webhoseio.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("webhoseio.Tests")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("webhoseio.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © webhoseio 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- webhoseio/Properties/AssemblyInfo.cs
+++ webhoseio/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("webhoseio")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("webhoseio")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("webhoseio")]
 [assembly: AssemblyProduct("webhoseio SDK")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © webhoseio 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
